095e960a6de4d89687ba3f85d7bcb034fd3aec2c
Peter <peter.pilestedt@gmail.com>
2021-09-06 10:41:12 +0200

    Initial commit

---
+++ src/main/java/hakimlivs/hakimmailservice/HakimMailServiceApplication.java
@@ -0,0 +1,13 @@
+package hakimlivs.hakimmailservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HakimMailServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HakimMailServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hakimlivs/hakimmailservice/HakimMailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package hakimlivs.hakimmailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HakimMailServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
